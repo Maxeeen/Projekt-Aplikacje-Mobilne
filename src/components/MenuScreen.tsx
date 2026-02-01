5ef907a31e172569da45390b9ccc23c930a80cd5
Maksymilian Ratajczak <mratajczak.pro@gmail.com>
2026-02-01 14:07:02 +0100

    added instructions on the bottom of MenuScreen, changing it to button might create less clutter in the future

--- src/components/MenuScreen.tsx
+++ src/components/MenuScreen.tsx
@@ -47,12 +47,18 @@ export default function MenuScreen({ gameMode, setGameMode, onStartGame }: MenuS
       <TouchableOpacity style={styles.mainButton} onPress={onStartGame}>
         <Text style={styles.buttonText}>ROZPOCZNIJ GRĘ</Text>
       </TouchableOpacity>
+        <View style={styles.footer}>
+          <Text style={styles.footerText}>Po rozpocząciu gry będziesz miał 15 sekund aby zaznaczyć na mapie miejsce pochodzenia potrawy</Text>
+          <Text style={styles.footerText2}>Pamiętaj że możesz użyć podpowiedzi aby wyświetlić nazwę potrawy</Text>
+          <Text style={styles.footerText}>Korzystając z podpowiedzi zdobędziesz 30% mniej punktów</Text>
+          <Text style={styles.footerText2}>Powodzenia!</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  menu: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#A0D1C1' },
+  menu: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#A0D1C1', paddingBottom: 80 },
   title: { fontSize: 36, fontWeight: 'bold', color: '#4A5284', marginBottom: 10, textAlign: 'center' },
   subtitle: { fontSize: 18, color: '#679CA6', marginBottom: 20 },
   modeContainer: { flexDirection: 'row', marginBottom: 40, backgroundColor: '#E8E4BC', borderRadius: 25, padding: 5 },
@@ -63,5 +69,8 @@ const styles = StyleSheet.create({
   modeTextActiveW: { color: '#519b01' },
   modeTextActiveEU: { color: '#FFCC00' },
   mainButton: { backgroundColor: '#E8E4BC', paddingVertical: 15, paddingHorizontal: 50, borderRadius: 30, elevation: 5 },
-  buttonText: { color: '#4A5284', fontSize: 18, fontWeight: 'bold' }
+  buttonText: { color: '#4A5284', fontSize: 18, fontWeight: 'bold' },
+  footer: { position: 'absolute', bottom: 70, paddingHorizontal: 20, alignItems: 'center' },
+  footerText: { fontSize: 18, color: '#4A5284', textAlign: 'center', marginVertical: 1 },
+  footerText2: { fontSize: 18, color: '#6A6A9C', textAlign: 'center', marginVertical: 8 },
 });
